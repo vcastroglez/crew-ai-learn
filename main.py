14dcda0d42346e1a3e6298a195ecd9a2b64b303c
Sanji / Vladimir Castro Gonzales <sanji@chibiphoenix.com>
2024-08-06 14:47:15 +0300

    single weather agent

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from langchain_community.llms.ollama import Ollama
 from crewai_tools import SerperDevTool
 import os
+import json
 
 load_dotenv()
 os.environ["OPENAI_API_KEY"] = "NA"
@@ -23,9 +24,9 @@ general_agent = Agent(role="Weather Anchor",
                       goal=f"""Give forecast of the weather in the city of {city}.""",
                       backstory=f"""You have been working in one of the main TV channels of {country} for 20 years and 
                       you are the leading Weather forecast anchor.""",
-                      max_iter=2,
                       allow_delegation=False,
-                      verbose=False,
+                      verbose=True,
+                      max_iter=1,
                       llm=llm)
 
 task = Task(description=f"""What is the weather in the city {city} of the country {country}""",
@@ -41,6 +42,12 @@ crew = Crew(
     verbose=2
 )
 
-result = crew.kickoff()
+crew_output = crew.kickoff()
 
-print(result)
+print(f"Raw Output: {crew_output.raw}")
+if crew_output.json_dict:
+    print(f"JSON Output: {json.dumps(crew_output.json_dict, indent=2)}")
+if crew_output.pydantic:
+    print(f"Pydantic Output: {crew_output.pydantic}")
+print(f"Tasks Output: {crew_output.tasks_output}")
+print(f"Token Usage: {crew_output.token_usage}")
