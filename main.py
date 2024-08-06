e10a26c59d614cb457b3bd0b00a0fb55d85c08b8
Sanji / Vladimir Castro Gonzales <sanji@chibiphoenix.com>
2024-08-06 14:35:25 +0300

    single general agent

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
 from crewai import Agent, Task, Crew
+from dotenv import load_dotenv
 from langchain_community.llms.ollama import Ollama
+from crewai_tools import SerperDevTool
 import os
 
+load_dotenv()
 os.environ["OPENAI_API_KEY"] = "NA"
+api_key = os.environ.get('SERPER_API_KEY')
+country = input("Which country: ")
+city = input("Which city: ")
+
+search_engine = SerperDevTool(
+    search_url=f"""https://google.serper.dev/search?api_key=${api_key}""",
+    n_results=2,
+)
 
 llm = Ollama(
     model="llama3.1:8b-instruct-q8_0",
     base_url="http://localhost:11434")
 
-general_agent = Agent(role="Math Professor",
-                      goal="""Provide the solution to the students that are asking mathematical questions and give them the answer.""",
-                      backstory="""You are an excellent math professor that likes to solve math questions in a way that everyone can understand your solution""",
+general_agent = Agent(role="Weather Anchor",
+                      goal=f"""Give forecast of the weather in the city of {city}.""",
+                      backstory=f"""You have been working in one of the main TV channels of {country} for 20 years and 
+                      you are the leading Weather forecast anchor.""",
+                      max_iter=2,
                       allow_delegation=False,
-                      verbose=True,
+                      verbose=False,
                       llm=llm)
 
-task = Task(description="""what is 3 + 5""",
+task = Task(description=f"""What is the weather in the city {city} of the country {country}""",
             agent=general_agent,
-            expected_output="A numerical answer.")
+            expected_output="A simple weather forecast, saying temperature in degrees Celsius, "
+                            "humidity and forecasts for the rest of the day. "
+                            "Use the most resent data available",
+            tools=[search_engine])
+
 crew = Crew(
     agents=[general_agent],
     tasks=[task],
